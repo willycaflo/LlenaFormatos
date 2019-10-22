d352b6881f4a7bf053cf74bcc5fb2b2d7d39a163
Guillermo Cabrera <gcabrera@cusa.canon.com>
2019-10-22 16:20:20 -0500

    Se crean librerias para poder hacer uso de la clase TinyButStrong y del plugin OpenTBS para trabajar con documentos de office

---
+++ application/libraries/MyTBS.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once APPPATH."third_party/tbs_us/tbs_class.php";
+
+class MyTBS extends clsTinyButStrong {
+    public function __construct() {
+        parent::__construct();
+    }
+}
+?>
---
+++ application/libraries/My_tbs_plugin.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once APPPATH."third_party/tbs_us/plugins/tbs_plugin_opentbs.php";
+
+class My_tbs_plugin extends clsTinyButStrong {
+    public function __construct() {
+        parent::__construct();
+    }
+}
+?>
